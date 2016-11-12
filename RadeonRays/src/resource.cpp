9038aef67275115cd73d4ca80519a2ee174ff620
Josh McNamee <josh04@gmail.com>
2016-11-12 15:58:43 +0000

    minor

--- RadeonRays/src/resource.cpp
+++ RadeonRays/src/resource.cpp
@@ -8,8 +8,19 @@
 
 #include <string>
 
-
-
+std::string _get_resource_path(const char * filename) {
+	std::string path = "RadeonResources/" + std::string(filename);
+#ifdef RAD_FRAMEWORK
+#ifdef __APPLE__
+	NSString * resStr = [[[NSBundle bundleWithIdentifier : @"josh04.RadeonRays"] resourcePath] stringByAppendingString:@" / "];
+		NSString * NSvertStr = [resStr stringByAppendingString : [NSString stringWithUTF8String : filename]];
+	path = [NSvertStr UTF8String];
+	return path;
+#endif
+#endif
+	return path;
+}
+/*
 const char * _get_resource_path(const char * filename) {
     const char * path = "../RadeonRays/src/kernels/";
     std::string output = std::string(path) + std::string(filename);
@@ -17,3 +28,4 @@ const char * _get_resource_path(const char * filename) {
 
     return path;
 }
+*/
